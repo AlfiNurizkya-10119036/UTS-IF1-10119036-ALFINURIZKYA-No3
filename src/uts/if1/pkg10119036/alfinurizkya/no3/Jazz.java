333e05c01e059b199aa0081d2a7aa5e9ca794ebd
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-12-04 21:26:59 +0700

    UTS No 3

---
+++ src/uts/if1/pkg10119036/alfinurizkya/no3/Jazz.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if1.pkg10119036.alfinurizkya.no3;
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * Nama     : Alfi Nurizkya
+ * NIM      : 10119036
+ * Kelas    : IF-1
+ * Deskripsi Program :
+ */
+public class Jazz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
